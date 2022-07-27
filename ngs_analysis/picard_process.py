8d11a87fd8bbe59afa5d2f44d6aea435a424b365
himanshusingh2018 <himanshu720@gmail.com>
2022-07-27 15:39:21 -0400

    add module picard_process, July 27, 2022

---
+++ ngs_analysis/picard_process.py
@@ -0,0 +1,52 @@
+import os
+def picard_bam_clean(idir, odir, sample):
+    '''
+    PICARD CLEANING SORT BAM FILE
+    
+    Reason: https://davetang.org/wiki/tiki-index.php?page=SAM#Clipped_alignment
+    
+            One query sequence may be aligned to multiple places on the reference genome, either with or without overlaps.
+            Hard clipping: Avoid presenting the full query sequence multiple times for non-overlapping hits
+            Soft clipping: these sequence are not part of the alignment. 
+            Therefore, soft clipping shall be removed. 
+    RUN:
+        picard_bam_clean(idir = '/path/to/bam/file/', odir='/path/to/output/dir/', sample=sample_name)
+
+    OUTPUT: /path/to/output/dir/sample.clean.bam
+
+    Picard CleanSam Arguments:
+        cmd: picard CleanSam --INPUT /bam/file/path/sample.sort.bam --OUTPUT /output/path/sample.clean.bam
+
+                CleanSam: Cleans the BAM, soft-clipping beyond-end-of-reference alignments and setting MAPQ (mapping quality) to 0 for unmapped reads 
+                            soft-clipping: bases in 5' and 3' of the read are NOT part of the alignment
+                            hard-clipping: bases in 5' and 3' of the read are NOT part of the alignment AND those bases have been removed from the read sequence in the BAM file.
+                INPUT   : Input, sort bam file
+                OUTPUT  : Output, clean bam file
+
+    '''
+    os.system(f"picard CleanSam --INPUT {idir}{sample}.sort.bam --OUTPUT {odir}{sample}.clean.bam")
+    print(f"{odir}{sample}.clean.bam is generated successfully...")
+
+def picard_markduplicate(idir, odir, sample):
+    '''
+    Mark duplicate reads.
+    Setting MAPQ to 0 for unmapped reads
+
+    Reason: duplicated mark reads are ignored by the post-processing. The duplicated reads should not removed, as they may impact the coverage.
+    
+    RUN: 
+        picard_markduplicate(idir='/path/to/bam/', odir='/path/to/bam/', sample='sample_name.clean.bam')
+    
+    OUTPUT: Two output files:
+                1. {/path/to/output/}{sample}.sort.mkdup.bam
+                2. {/path/to/output/}{sample}.sort.dup.metrics.txt
+
+    Picard Markduplicate Arguments:
+        --INPUT                 : input SAM, BAM or CRAM files to analyze. Must be coordinate sorted.
+        --OUTPUT                : output file to write marked records
+        --METRICS_FILE          : File to write duplication metrics to  Required.  
+        --VALIDATION_STRINGENCY : {STRICT, LENIENT, SILENT}. LENIENT Setting stringency to SILENT can improve performance when processing a BAM file in which variable-length data (read, qualities, tags) do not otherwise need to be decoded.  Default value: STRICT.
+        --CREATE_INDEX          : {true|false}, Whether to create an index when writing VCF or coordinate sorted BAM output.  Default value: false. Possible values: {true, false} 
+    '''
+    os.system(f"picard MarkDuplicates --INPUT {idir}{sample}.clean.bam --OUTPUT {odir}{sample}.sort.mkdup.bam --METRICS_FILE {odir}{sample}.sort.dup.metrics.txt --VALIDATION_STRINGENCY LENIENT --CREATE_INDEX true")
+    print(f"Successfully generated files: \n\t1: {odir}{sample}.sort.mkdup.bam \n\t2: {odir}{sample}.sort.dup.metrics.txt")
--- ngs_analysis/read_align.py
+++ ngs_analysis/read_align.py
@@ -1,71 +1,90 @@
 import os
 
-def bwa_align(idir, fin, genome_size_in_gb, nthreads):
-  '''
-  BWA Genome Indexing (preferred for variant analysis)
-  http://bio-bwa.sourceforge.net/bwa.shtml
+def bwa_fastq_aln(idir, odir, genome_index, reads, sample, nthreads):
+    '''
+    BWA MEM FastQ Alignment (preferred for variant )
+        
+        Variant Analysis Pipeline: https://www.illumina.com/products/by-type/informatics-products/basespace-sequence-hub/apps/bwa-aligner.html
+        Read Groups: https://gatk.broadinstitute.org/hc/en-us/articles/360035890671-Read-groups
+            RG: Read Group; ID: Read Group Identifier; PU: Platform Unit; SM: Sample; PL: Platform/technoloty used to produce the read; LB: DNA preparation library
 
-  Run:
-    bwa index -a is|bwtsw {idir=/path/to/}{fin = genome.fa} -p -t {nthreads = 5}
-  
-  Parameters:
-    index: Index genome sequence in fasta format
-    -a   : Alogorithm for constructing BWT index. Two algorithms bwtsw and is
-    is   : Genome size < 2GB
-    bwtsw: Genome size >= 2GB. 
-    -p   : Prefix of the output database, same as genome file name. e.g. GRCh38.primary_assembly.genome.fa
-    -t   : number of threads
+        BWA MEM Alignment Command:
+            bwa mem -t {10} -R {RG} {genome_index} {r1.fastq (SE) | r1.fq r2.fq (PE)} | samtools view -bS --threads 10| samtools sort -o {/path/output/dir/r.sort.bam --threads 10}
+                mem: 
+                -t : number of threads
+                -R : read group header line such as '@RG\tID:foo\tSM:bar'
+            samtools:
+                view: print all alignments if with no options
+                -b  : Output BAM
+                 S  : Ignored (input format is auto-detected)
+                sort: Sort alignments by leftmost coordinates
+                -o  : output e.g. sample.sort.bam
+    Run:
+        bwa_fastq_aln(genome_index = /path/to/genome_index/index.fa, reads = ['r1.fq', 'r2.fq], 
+                        odir = '/output/path/', sample = 'sample_name', nthreads = 10)
+
+    Arguments:
+        genome_index: /path/to/bwa/genome/index_name
+        reads       : list of reads. e.g. ['r1.fq'] or ['r1.fq', 'r2.fq']
+        odir        : /output/dir/path/
+        sample      : sample_name e.g. 'r'
+        nthreads    :  10
     
-  Arguments:
-    idir : Genome fasta file directory path. e.g. /path/to/genome/
-    fin  : Genome fasta file name. e.g. genome.fa
-    genome_size_in_gb: 3 (Human genome size: 3.2 GB)
-    nthreads: number of threads
-  
-  Output: Five output file will generated at the same location of genome fasta file with same name
-          1. GRCh38.primary_assembly.genome.fa.amb
-          2. GRCh38.primary_assembly.genome.fa.ann
-          3. GRCh38.primary_assembly.genome.fa.bwt
-          4. GRCh38.primary_assembly.genome.fa.pac
-          5. GRCh38.primary_assembly.genome.fa.sa
-
-  '''
-  print(f'bowtie2 -t -x bowtie2/NC_012967.1 -1 SRR030257_1.fastq -2 SRR030257_2.fastq -S bowtie2/SRR030257.sam')
-
-
-def bwa_fastq_aln(genome_index, read1, read2, fout, sample, nthreads):
-    """ 
-    STEP 2: BWA MEM FastaQ Alignment
-    """    
-    os.makedirs(fout, exist_ok=True)
-    #fastq alignment
+    Output:
+        /output/dir/path/sample.sort.bam
     '''
-    bwa mem -t 24 -R "@RG\tID:P-0000879-N01-WES_IGO_12502_F_25_S21_L001" /home/singhh5/hg38/grch38_gencode_bwa_index/GRCh38.primary_assembly.genome.fa fastq/P-0000879-N01-WES_IGO_12502_F_25_S21_L001_R1_001.fastq.gz fastq/P-0000879-N01-WES_IGO_12502_F_25_S21_L001_R2_001.fastq.gz | samtools view -bS | samtools sort -o bwa_aln/P-0000879-N01-WES_IGO_12502_F_25_S21_L001.sort.bam
-    '''
-    #RG = "@RG\tID:{sample}_PE\tPL:Illumina\tPU:{sample}\tLB:{sample}\tSM:{sample}"#bam header
-    RG = f"'@RG\\tID:{sample}_PE\\tPL:Illumina\\tPU:{sample}\\tLB:{sample}\\tSM:{sample}'"#bam header
-    aln = f"bwa mem -t {nthreads} -R {RG} {genome_index} {read1} {read2} | samtools view -bS | samtools sort -o {fout}{sample}.sort.bam"
-    print(aln)
-    os.system(aln)
-    exit()
-    #indexing bam file
-    os.system(f"samtools index {fout}{sample}.sort.bam")
+    os.makedirs(odir, exist_ok=True)#create output directory
     
-    print(f'{sample} alignment and indexing is completed successfully...')
+    if(len(reads) == 1):
+        RG = f"'@RG\\tID:{sample}_SE\\tPL:Illumina\\tPU:{sample}\\tLB:{sample}\\tSM:{sample}'"#ID: SE, bam header
+        os.system(f"bwa mem -t {nthreads} -R {RG} {genome_index} {idir}{reads[0]} | samtools view -bS --threads {nthreads} | samtools sort -o {odir}{sample}.sort.bam --threads {nthreads}")
+        print('Single-End alignment:\n\t{odir}{sample}.sort.bam')
+    else:
+        RG = f"'@RG\\tID:{sample}_PE\\tPL:Illumina\\tPU:{sample}\\tLB:{sample}\\tSM:{sample}'"#ID: PE, bam header
+        os.system(f"bwa mem -t {nthreads} -R {RG} {genome_index} {idir}{reads[0]} {idir}{reads[1]} | samtools view -bS | samtools sort -o {odir}{sample}.sort.bam")
+        print('Paired-End alignment:\n\t{odir}{sample}.sort.bam')
+    
+#bwa_fastq_aln(genome_index='/Volumes/lilac_home_singhh5/hg38/grch38_gencode_bwa_index/GRCh38.primary_assembly.genome.fa', reads=['fastq/EZ-123-B_IGO_08138_J_2_S101_R1_001.fastq.gz'], odir='./', sample='E', nthreads=4)
 
-def picard_clean_n_markduplicate(sample, bwa_aln):
+def bowtie2_fastq_aln(idir, odir, genome_index, reads, sample, nthreads):
     '''
-    PICARD CLEANING SORT BAM FILE
-    setting MAPQ to 0 for unmapped reads
+    bowtie2: FastQ Alignemnt (prefer other than Variant Calling)
+    https://rnnh.github.io/bioinfo-notebook/docs/bowtie2.html
+    
+        bowtie2 --no-unal --threads 4 -x genome_index_name -1 reads_1.fastq -2 reads_2.fastq | samtools view -bS --threads 4 | samtools sort -o {/path/outdir/}{sample}.sort.bam --threads 4
+            --no-unal   : (optional) reads that do not align to the reference genome will not be written to sam output
+            --threads   : number of threads
+            -x          : genome index
+            -1          : read1.fq
+            -2          : read2.fq
+    
+    RUN: 
+        bowtie2_fastq_aln(idir = '/path/bam/dir/', odir='/path/outdir/', genome_index = '/bowtie2/index/name', reads['r1.fq','r2.fq'], sample='sample')
+    
+    Arguments:
+            idir            : /dir/path/input/bam
+            odir            : /outdir/path/
+            genome_index    : /path/bowtie2/index/GRCh38, bowtie2 genome index name 'GRCh38'
+            reads           : list of read sequence, {['r1.fq'] or ['r1.fq', 'r2.fq']} 
+            sameple         : name of sample
+    
+    Output:
+        /outdir/path/sample.sort.bam
     '''
-    os.system(f"picard CleanSam --INPUT {bwa_aln}{sample}.sort.bam --OUTPUT {bwa_aln}{sample}.clean.bam")
-    print(f"{bwa_aln}{sample}.clean.bam is generated successfully...")
-    os.system(f"picard MarkDuplicates --INPUT {bwa_aln}{sample}.clean.bam --OUTPUT {bwa_aln}{sample}.mkdup.bam --METRICS_FILE {bwa_aln}{sample}.dup.metrics.txt --VALIDATION_STRINGENCY LENIENT --CREATE_INDEX true")
-    print(f"Successfully generated files: \n\t1: {bwa_aln}{sample}.sort.mkdup.bam \n\t2:{fout}{sample}.dup.metrics.txt")
+    os.makedirs(odir, exist_ok=True)#create output directory
+    if(len(reads) == 1):
+        RG = f"'@RG\\tID:{sample}_SE\\tPL:Illumina\\tPU:{sample}\\tLB:{sample}\\tSM:{sample}'"#ID: SE, bam header
+        os.system("bowtie2 --no-unal --threads {nthreads} --rg {RG} -x {genome_index} {idir}{reads[0]} | samtools view -bS --threads {nthreads} | samtools sort -o {odir}{sample}.sort.bam --threads {nthreads}")
+        print('Single-End alignment:\n\t{odir}{sample}.sort.bam')
+    else:
+        RG = f"'@RG\\tID:{sample}_SE\\tPL:Illumina\\tPU:{sample}\\tLB:{sample}\\tSM:{sample}'"#ID: SE, bam header
+        os.system("bowtie2 --no-unal --threads {nthreads} --rg {RG} -x {genome_index} -1 {idir}{reads[0]} -2 {idir}{reads[1]} | samtools view -bS --threads {nthreads} | samtools sort -o {odir}{sample}.sort.bam --threads {nthreads}")
+        print('Paired-End alignment:\n\t{odir}{sample}.sort.bam')
 
-def gatk_variant_calling_sample(sample, fout, nthreads):
-    '''
-    GATK Mutect2 Variant Calling
-    '''
-    os.system(f"gatk Mutect2 --native-pair-hmm-threads {nthreads} -R {genome_index} -I {fout}{sample}_R1_001.sort.bam -O {fout}{sample}.vcf.gz")
-    print('{sample} is generated successfully:\n\t{fout}{sample}.vcf.gz')
+#bowtie2_fastq_aln(idir='./', odir='./', genome_index='GRCh38_noalt_as/GRCh38_noalt_as', reads=['fastq/EZ-123-B_IGO_08138_J_2_S101_R1_001.fastq.gz'], sample='E', nthreads=4)    
+
+def star_rnaseq_fastq_2pass_aln():
+    pass
+
+def star_rnaseq_fastq_1pass_aln():
+    pass
