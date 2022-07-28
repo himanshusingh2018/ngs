fad5980d895e9212953052f55c6eaa1c0d624db7
himanshusingh2018 <himanshu720@gmail.com>
2022-07-28 11:17:45 -0400

    sample_vs_gene_mut_table module, July 28, 2022

--- misc/sample_vs_gene_mut_table.py
+++ misc/sample_vs_gene_mut_table.py
@@ -1,6 +1,7 @@
 import pandas as pd
 def funcotator_anot_vcf_mut_gene_table(vcf):
     '''
+    RUN: funcotator_anot_vcf_mut_gene_table('file.vcf')
     Input: Funcotator VCF file
     Output: 
         fname   nGene   nMutation
@@ -9,12 +10,9 @@ def funcotator_anot_vcf_mut_gene_table(vcf):
     '''
     df = pd.read_csv(vcf, sep="\t", header=0, skiprows=266, usecols=['INFO'], encoding='latin-1')
     df = pd.DataFrame(df.INFO.str.split('\=\[').str[1].str.split('|').str[0])
-    #print(df)
     data = [vcf.split('/')[-1], df.size, len(df.drop_duplicates()), len(df[df.INFO=='Unknown'])]
-    table = pd.DataFrame(data,
+    table = pd.DataFrame([data],
                          columns=['sample', 'num_genes', 'num_mutation', 'unannotated_mutation'])
-    #print(f.split('/')[-1], len(df.drop_duplicates()), df.size, len(df[df.INFO=='Unknown']))
-    print(table)
     return(table)
 
 funcotator_anot_vcf_mut_gene_table(vcf='/Volumes/lilac_data_ziv/transciptome/paired_pnet/Project_12502_F/anotvcf/14_3_IGO_12502_F_9_S45_L002.pass.anot.vcf')
