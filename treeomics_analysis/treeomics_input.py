1fd701e7d6ebd630aca2fe6d110b4733aa0b6286
Himanshu Narayan Singh <46487597+himanshusingh2018@users.noreply.github.com>
2022-08-18 13:21:41 -0400

    treeomics_input_vcffilter, Aut 18, 2022
    d.assign(ALT=d.ALT.str.split(',')).explode('ALT').reset_index(drop=True)# all comma separated values in ALT col in to new rows

--- treeomics_analysis/treeomics_input.py
+++ treeomics_analysis/treeomics_input.py
@@ -76,9 +76,9 @@ def treeomics_input_vcffilter(vcfdir='vcf', odir='./'):
     OUTPUT:
     Generate Two Input files for TREEOMICS from Annotated VCF file
 
-    1. <mut_reads.txt> mut-reads: number of reads reporting a variant (row) in each sample
-       AD (Allele Depth) in VCF file (e.g. AD: 1,15; 1: ref and 15: alt)
-       AD = 15
+        1. <mut_reads.txt> mut-reads: number of reads reporting a variant (row) in each sample
+            AD (Allele Depth) in VCF file (e.g. AD: 1,15; 1: ref and 15: alt)
+            AD = 15
 
             mut-reads table: tab-seperated
                 Columns: Chromosome Position    Change  Gene    Sample1 Sample2 Sample3
@@ -86,16 +86,15 @@ def treeomics_input_vcffilter(vcfdir='vcf', odir='./'):
                          6	        25885022	T>C	    SLC17A4	n/a	    n/a	    55792
                          19	        49523688	T>A	    ZFP112	32218	39057	59962
 
-    2. coverage: tab-separated-value file with the sequencing depth at the position of this variant in each sample
-       DP (Total Depth) in VCF file (e.g. DP: 16). In the Gene column 'gene' is used suggests: no annotation
-       DP = 16
+        2. coverage: tab-separated-value file with the sequencing depth at the position of this variant in each sample
+            DP (Total Depth) in VCF file (e.g. DP: 16). In the Gene column 'gene' is used suggests: no annotation
+            DP = 16
 
             coverage table: tab-seperated
                 Columns: Chromosome Position    Change  Gene    Sample1 Sample2 Sample3
                          17	        4639699	    A>T	    gene	n/a	    n/a	    34560
                          6	        25885022	T>C	    gene	n/a	    n/a	    55792
                          19	        49523688	T>A	    gene	32218	39057	59962
-
     '''
     samp = [os.path.join(vcfdir, f) for f in os.listdir(vcfdir) if f.endswith('.vcf')] #read all vcf files from folder
     df = pd.DataFrame() #final treeomics input files dataframe
@@ -103,15 +102,17 @@ def treeomics_input_vcffilter(vcfdir='vcf', odir='./'):
     for s in samp:#extracting Allelic Depth (AD) and Total Depth (DP) from each vcf files and merging together
         print(f'Reading {s}...')
         d = pd.read_csv(s, sep="\t", header=0, comment = '##', engine='python', encoding = 'unicode_escape')
-        d = d[d.FILTER == 'PASS'] #Extract variant with Filter: 'PASS'
+        #d = d[d.FILTER == 'PASS'] #Extract variant with Filter: 'PASS'
         d = d[ ['#CHROM', 'POS', 'REF', 'ALT', 'INFO', 'FORMAT', d.columns[-1]] ]
         sname = d.columns[-1] #sample name
         d[f'{sname}AD'] = d[sname].str.split(':').str[1].str.split(',').str[1] #extract Allelic Depth (AD)
         d[f'{sname}DP'] = d[sname].str.split(':').str[3] #extract Total Depth (DP)
+        d = d.assign(ALT=d.ALT.str.split(',')).explode('ALT').reset_index(drop=True)#all comma separated values in ALT in new rows
+
         d['Gene'] = 'gene' #No gene annotation so 'gene' is used
         #d['Gene'] = d.INFO.str.split('[').str[1].str.split('|').str[0] #Extract Gene Symbol
         d['Change'] = d['REF'].astype(str) + '>' + d['ALT'].astype(str) #Merging Ref > Alt
-            
+        
         if(i == 1):#Only First VCF file Extracted information merge with empty df 
             df = d[['#CHROM', 'POS', 'Change', 'Gene', f'{sname}AD', f'{sname}DP']]
             i+=1
