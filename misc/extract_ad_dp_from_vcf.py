61953ae554cd5372eae062d944757edf299fd8c5
Himanshu Narayan Singh <46487597+himanshusingh2018@users.noreply.github.com>
2023-05-16 13:57:13 -0400

    updated for multisample vcf file extract_ad_dp_from_vcf.py

--- misc/extract_ad_dp_from_vcf.py
+++ misc/extract_ad_dp_from_vcf.py
@@ -1,6 +1,7 @@
-import os, pandas as pd, gzip
+import os, gzip
+import pandas as pd
 
-def extract_ad_pd(idir,odir,chr,start,end,fout):
+def extract_ad_pd(idir, odir, chr, start, end, fout):
     '''
     Extract Allelic Depth and Depth from filtered VCFs in DIR
     INPUT:
@@ -12,30 +13,43 @@ def extract_ad_pd(idir,odir,chr,start,end,fout):
         fout: name of output file
     OUTPUT:
         {odir}all_snp.txt #AD, AF, DP of all SNP position in samples vcf in {idir}
-        {odir}{fout}.txt #AD, AF, DP of SNPs in user defined genomic regions e.g. chr17, start, end
+        {odir}{fout}.txt #AD, AF, DP of SNPs in user-defined genomic regions e.g. chr17, start, end
     RUN:
-        extract_ad_pd(idir='filter/',odir='./',chr='chr17',start=7569720,end=7592868)
+        extract_ad_pd(idir='filter/', odir='./', chr='chr17', start=7569720, end=7592868, fout='table.txt')
     '''
     files = [f for f in os.listdir(idir) if f.endswith('.filter.vcf.gz')]
     table = pd.DataFrame()
     for f in files:
-        cols = ['CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT',f.split('.')[0]]
+        cols = line = next((line.rstrip('\n').split('\t') for line in gzip.open(f'filter/{f}', 'rt', encoding='latin-1') if line.strip().startswith('#CHROM')), None)
         df = pd.read_csv(f'{idir}{f}', sep='\t', comment='#', header=None, names=cols)
-        df = df.assign(AD=df[f.split('.')[0]].str.split(':').str[1], 
-                       AF=df[f.split('.')[0]].str.split(':').str[2], 
-                       DP=df[f.split('.')[0]].str.split(':').str[3])
-        df.rename(columns={'AD': 'AD_'+f.split('.')[0],
-                           'AF': 'AF_'+f.split('.')[0],
-                           'DP': 'DP_'+f.split('.')[0]}, inplace=True)
+        for i in range(9, len(cols)):#iterate over each sample in vcf file
+            df = df.assign(
+                AD=df[cols[i].split('.')[0]].str.split(':').str[1],
+                AF=df[cols[i].split('.')[0]].str.split(':').str[2],
+                DP=df[cols[i].split('.')[0]].str.split(':').str[3]
+            )
+            df.rename(
+                columns={
+                    'AD': 'AD_'+cols[i],
+                    'AF': 'AF_'+cols[i],
+                    'DP': 'DP_'+cols[i]
+                },
+                inplace=True
+            )
         df = df.filter(regex='CHROM|POS|AD|AF|DP')
         
-        if table.empty: table = df
-        else: table = pd.merge(table, df, on=['CHROM', 'POS'], how='outer')#; print(table);exit()
-    table.to_csv(f'{odir}all_snps.txt',header=True,index=False)
-    
-    condition = (table['CHROM'] == chr[3:]) & (table['POS'] >= start) & (table['POS'] <= end)
-    table[condition].to_csv(f'{odir}{fout}',sep="\t",header=True,index=False)
-    print(f'files generated: \n\t{odir}{fout}\n\t{odir}all_snps.txt')
+        df['POS'] = df['POS'].astype(int)  # Convert 'POS' column to int64 data type
+        
+        if table.empty:
+            table = df
+        else:
+            table = pd.merge(table, df, on=['#CHROM', 'POS'], how='outer')
 
-    
-#extract_ad_pd(idir='filter/',odir='./',chr='chr17',start=7569720,end=7592868,fout='table.txt')
+    table.to_csv(f'{odir}all_snps.txt', header=True, index=False)
+
+    condition = (table['#CHROM'] == chr[3:]) & (table['POS'] >= start) & (table['POS'] <= end)
+    table[condition].to_csv(f'{odir}{fout}', sep="\t", header=True, index=False)
+    print(f'Files generated:\n\t{odir}{fout}\n\t{odir}all_snps.txt')
+
+
+#extract_ad_pd(idir='filter/', odir='./', chr='chr17', start=7569720, end=7592868, fout='table.txt')
